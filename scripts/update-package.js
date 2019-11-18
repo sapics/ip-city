81549161da8bf4df626a1dfbb8f33aa465dbcee2
sapics <gv.nishino@gmail.com>
2019-11-18 09:11:09 +0900

    json prettier

--- scripts/update-package.js
+++ scripts/update-package.js
@@ -6,5 +6,5 @@ var stat2 = fs.statSync('data/geoip-country6.dat')
 if(stat1.size > 1024 * 1024 && stat2.size > 2 * 1024 * 1024){
 	var json = JSON.parse(fs.readFileSync('package.json', 'utf8'))
 	json.version = json.version.replace(/(\d+)$/, function(r){return (r|0)+1})
-	fs.writeFileSync('package.json', JSON.stringify(json))
+	fs.writeFileSync('package.json', JSON.stringify(json, null, "\t"))
 }
